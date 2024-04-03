583b423f1fd7b05c29e1d6919c312095949940bf
Sierra Sutherland-Brown <sierrabardot@live.com.au>
2024-04-03 18:18:30 +1100

    Start styling hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { ContactSection } from './components/ContactSection/ContactSection';
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,36 @@
 import { NavLink } from 'react-router-dom';
 import { NavbarProps } from '../../types';
+import './Navbar.css';
 
 export const Navbar: React.FC<NavbarProps> = ({ setProfile }) => {
     const handleClick = (value: boolean) => setProfile(value);
     return (
-        <div>
-            <ul>
-                <NavLink to='/' onClick={() => handleClick(true)}>
-                    Profile
-                </NavLink>
-                <NavLink to='/projects' onClick={() => handleClick(false)}>
-                    Projects
-                </NavLink>
-                <NavLink to='/contact' onClick={() => handleClick(false)}>
-                    Contact
-                </NavLink>
+        <div className='navbar navbar-expand justify-content-center'>
+            <ul className='navbar-nav fw-semibold   mb-3'>
+                <li className='nav-item'>
+                    <NavLink
+                        className={'nav-link'}
+                        to='/'
+                        onClick={() => handleClick(true)}>
+                        Profile
+                    </NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink
+                        className={'nav-link'}
+                        to='/projects'
+                        onClick={() => handleClick(false)}>
+                        Projects
+                    </NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink
+                        className={'nav-link'}
+                        to='/contact'
+                        onClick={() => handleClick(false)}>
+                        Contact
+                    </NavLink>
+                </li>
             </ul>
         </div>
     );
