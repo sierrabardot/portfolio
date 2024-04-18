0f3b5b6d48f0eea716f79ef7a6ef12b1ff1f08ee
Sierra Sutherland-Brown <sierrabardot@live.com.au>
2024-04-18 18:47:42 +1000

    Update contact details

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,11 +1,33 @@
+import styles from './ContactSection.module.css';
+
 export const ContactSection: React.FC = () => {
     return (
         <div className='container-fluid d-flex flex-column px-4 px-md-5 mb-4'>
-            <h1 className='mb-4'>Contact</h1>
-            <ul>
-                <li>mobile: 0411 512 026</li>
-                <li>email: sierrasutherlandbrown@gmail.com</li>
-            </ul>
+            <div className='row mb-5 d-flex align-items-center justify-content-center'>
+                <div className='col-md-7 pe-md-5'>
+                    <h1 className={`${styles.heading} my-4`}>Contact</h1>
+                    <p>
+                        I'm on the lookout for a new opportunity to continue
+                        learning and developing my skills. Please reach out to
+                        discuss potential collaborations or opportunities for
+                        growth.
+                    </p>
+                    <p>
+                        <span className='fw-bolder'>mobile:</span> 0411 512 026
+                    </p>
+                    <p>
+                        <span className='fw-bolder'>email:</span>{' '}
+                        sierrasutherlandbrown@gmail.com
+                    </p>
+                </div>
+                <div className='col-md-5 col-8 ps-md-5'>
+                    <img
+                        src='icons/woman-on-laptop.svg'
+                        className='w-100 p-4'
+                        alt='Woman sitting at a desk with laptop open'
+                    />
+                </div>
+            </div>
         </div>
     );
 };
